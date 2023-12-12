67c2ab395509e8cb8367cb6b56aa3f45cbf54c06
Jan Titzeck <jan.titzeck@tu-dortmund.de>
2023-12-12 23:11:48 +0100

    12 part 2

--- 12.py
+++ 12.py
@@ -1,28 +1,29 @@
 
 import re
-
+from functools import cache
 
 def group_fits_at(size, i, row):
     return (
         i + size <= len(row) # group fits in rows length
-        and (i == 0 or row[i-1] in ['.', '?']) # before i is free space
+        and (i == 0 or row[i-1] in ['.', '?']) # before first spring is no spring
         and all(x in ['#', '?'] for x in row[i:i+size]) # springs can be placed in [i, i+size)
-        and (i + size == len(row) or row[i+size] in ['.', '?']) # after i + size is free space
+        and (i + size == len(row) or row[i+size] in ['.', '?']) # after last spring is no spring
     )
 
-def f(i, groups, row):
-    if groups == []: 
-        return 1
+@cache
+def arrangements(i, groups, row):
+    if groups == (): 
+        return all([x in ['.', '?'] for x in row[i:]])
     sum = 0
     for j in range(i, len(row)):
         if group_fits_at(groups[0], j, row):
-            sum += f(j + groups[0] + 1, groups[1:], row)
+            sum += arrangements(j + groups[0] + 1, groups[1:], row)
         if row[j] == '#': break
     return sum
 
 
 input = [re.match(r"([?.#]+) ([\d,]*)", l).groups() for l in open("input.txt", "r").readlines()]
 input = [(r, [int(i) for i in re.findall(r'\d+', g)]) for (r, g) in input]
-arrangements = [f(0, g, r) for (r, g) in input]
-print(sum(arrangements))
-#print(f(0, [3], '?###????????'))
+
+arrs = [arrangements(0, tuple(g * 5), "?".join([r] * 5)) for r, g in input]
+print(sum(arrs))
